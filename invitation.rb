1dc7f6283ffd26d5ce300892789ea908a51c6c9c
Alexsia Light <elightl729@comcast.net>
2018-07-23 20:21:37 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -20,3 +20,11 @@ time = gets.chomp
 puts "who is hosting the party"
 
 host_name = gets.chomp
+
+
+
+puts "Dear #{guest_name},"
+puts "You are cordiallu invited to the #{party_name} on #{date} at #{time}."
+puts "Please RSVP no later than October 30."
+puts "sincerely,"
+#{host_name}
